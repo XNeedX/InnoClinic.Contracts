66f73a61343c5d765940894abb7d3a9d9a9590b3
NeedX <hramko2016@yandex.ru>
2026-05-22 11:36:56 +0300

    final fix v 1.0.1

--- InnoClinic.Contracts/Events/Services/ISpecializationStatusCommandUpdate.cs
+++
@@ -1,9 +0,0 @@
-﻿using InnoClinic.Contracts.Enums;
-
-namespace InnoClinic.Contracts.Events.Services;
-
-public interface ISpecializationStatusUpdatedEvent
-{
-    Guid Id { get; }
-    ServiceStatus Status { get; }
-}
