b353dd5bc0af9edf43d2f1b630ace93498c62f0f
NeedX <hramko2016@yandex.ru>
2026-06-03 11:28:27 +0300

    feat: added pdf publication event

---
+++ InnoClinic.Contracts/Events/Appointments/ISaveAppointmentResultDocumentEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace InnoClinic.Contracts.Events.Appointments;
+
+public interface ISaveAppointmentResultDocumentEvent
+{
+    Guid AppointmentId { get; }
+    byte[] PdfBytes { get; }
+    string ContentType { get; }
+}
